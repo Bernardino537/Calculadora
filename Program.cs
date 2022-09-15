345b1f0d55c1209cb4a39fc56b92df4d41add588
Bruno de Oliveira Bernardino <112024859+Bernardino537@users.noreply.github.com>
2022-09-15 00:39:13 -0300

    Add files via upload
    funcoes calculadora

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal a = Convert.ToDecimal(Console.ReadLine());
+            decimal b = Convert.ToDecimal(Console.ReadLine());
+
+            
+
+            void SomaCalculadora(decimal num1, decimal num2)
+            {
+                Console.WriteLine($"a soma é {num1 + num2}");
+            }
+
+            SomaCalculadora(a, b);
+            
+        }
+    }
+}
+
